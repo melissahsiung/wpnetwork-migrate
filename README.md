a1e382e80c1d63d9148a2c6b4a3fb4cc939c422e
melissahsiung <web@fieldandpostbk.com>
2015-07-19 11:27:37 -0400

    added script

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # wpnetwork-db-migrate
 a php script for updating the domain when migrating a WordPress multisite installation
+
+## Warnings!
+- This has only been tested (once) on a multisite installation with subfolders
+
+## Requests
+* Please comment/fork/email: melissa@fieldandpostbk.com
+
+## How to use this script
+- Change the variables in the script 
+- Run it
+- Copy the output, paste it as a SQL query in phpMyAdmin
+
+More details on all the other stuff you have to do:
+http://melissahsiung.com/cloning-wordpress-multisite/
+
+
+## Future plans
+- Make a form for the vars
+- Connect/modify the db
+
+## Log
+* v.0.1: It worked! Twice!
---
+++ db-sqlscript.php
@@ -0,0 +1,68 @@
+<html>
+<body>
+
+<?php 
+
+// v.0.1
+// use this script at your own risk!
+// i've only tested it once so far.
+// good luck!
+//
+// melissa@fieldanpdpostbk.com
+
+// update these variables
+$numSites = 18; // including the main site
+$dbName = 'localDB';
+$oldDomain = 'liveproject.com';
+$newDomain = 'localproject.dev';
+
+
+// you don't need to do anything else
+$numChildSites = $numSites - 1;
+$sqlQuery = '';
+
+// update site tables
+$sqlQuery = "
+   UPDATE `{$dbName}`.`wp_sitemeta` 
+      SET `meta_value` = 'http://{$newDomain}/' 
+      WHERE `wp_sitemeta`.`meta_key` = 'siteurl';
+
+   UPDATE `{$dbName}`.`wp_site` 
+      SET `domain` = '{$newDomain}' 
+      WHERE `wp_site`.`domain` LIKE '%{$oldDomain}';
+
+   UPDATE `{$dbName}`.`wp_options` 
+      SET `option_value` = 'http://{$newDomain}' 
+      WHERE `wp_options`.`option_name` = 'siteurl';
+
+   UPDATE `{$dbName}`.`wp_options` 
+      SET `option_value` = 'http://{$newDomain}' 
+      WHERE `wp_options`.`option_name` = 'home';
+
+   UPDATE `{$dbName}`.`wp_blogs` 
+      SET `domain` = '{$newDomain}';
+
+   ";
+
+
+// update blog tables
+for ($i=0; $i < $numChildSites; $i++) {
+   $blogID = $i+2;
+   $blogTable = // "{$blogID} says hi<br>";
+      "UPDATE `{$dbName}`.`wp_{$blogID}_options` 
+         SET `option_value` = REPLACE(`option_value`, '{$oldDomain}', '{$newDomain}') 
+         WHERE `wp_{$blogID}_options`.`option_name` = 'siteurl';
+      UPDATE `{$dbName}`.`wp_{$blogID}_options` 
+         SET `option_value` = REPLACE(`option_value`, '{$oldDomain}', '{$newDomain}') 
+         WHERE `wp_{$blogID}_options`.`option_name` = 'home';
+      
+      ";
+
+   $sqlQuery = $sqlQuery . $blogTable;
+}
+
+echo "<pre>" . $sqlQuery . "</pre>";
+
+?>
+</body>
+</html>
